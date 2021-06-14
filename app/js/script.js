f6407bc3572c56bb49a6fb7e4cb1c1f12d12bb18
Simon <gagnonbsimon@gmail.com>
2021-06-14 16:01:59 -0400

    finished mobile menu overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,17 @@
-console.log("test");
 
+const btnBurger = document.querySelector('.header__hamburger');
+const body = document.querySelector('body');
+const overlay = document.querySelector('.header__links');
+
+btnBurger.addEventListener('click', function(){
+
+    if (!btnBurger.classList.contains('open')) {
+        btnBurger.classList.add('open');
+        overlay.classList.add('show');
+        body.classList.add('noscroll');
+    } else {
+        btnBurger.classList.remove('open');
+        overlay.classList.remove('show');
+        body.classList.remove('noscroll');
+    }
+});
